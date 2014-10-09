7d83ed8caae9a33c4422f5fb9eb93535d4d1d07e
Bogdan Melnik <ld@hackerdom.ru>
2014-10-10 01:13:33 +0200

    First version of scoreboard

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 
 app.use(morgan('dev'))
 
-app.get('/re/14/test', routes.ratings);
+app.get('/re/14/ratings.html', routes.ratings);
 
 app.listen(8080, "localhost", function() {
     console.log("I listen you.")
