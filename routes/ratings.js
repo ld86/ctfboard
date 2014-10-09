fb87554e5e80019986476f7f2e0b79a1f45c99cf
Bogdan Melnik <ld@hackerdom.ru>
2014-10-10 03:35:06 +0600

    Collect tasks and users

--- routes/ratings.js
+++
@@ -1,8 +0,0 @@
-var redis = require("redis"),
-    client = redis.createClient()
-
-exports.index = function(req, res) {
-    client.get('tasks', function(err, reply) {
-        res.render('index', JSON.parse(reply));
-    });
-}
