927e6fb62143bb1b39a4225df2ccb1f5b72a16aa
Bogdan Melnik <ld@hackerdom.ru>
2014-10-10 00:11:35 +0200

    First scoreboard with sorting

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var redis = require("redis"),
     client = redis.createClient()
 
-exports.index = function(req, res) {
-    client.get('tasks', function(err, tasks) {
-    client.get('users', function(err, users) {
-        tasks = JSON.parse(tasks)
-        users = JSON.parse(users)
-        variables = { 'tasks' : tasks['tasks'], 'users' : users['users']}
 
-        res.render('index', variables)
-    }});
+exports.ratings = function(req, res) {
+    client.get('tasks', function(err, tasks) {
+        client.get('users', function(err, users) {
+            tasks = JSON.parse(tasks);
+            users = JSON.parse(users);
+
+            sorted = [];
+            for (var i = 0; i < users.users.length; ++i) {
+                var totalScore = 0;
+                for (var taskName in users.users[i].tasks) {
+                    totalScore += users.users[i].tasks[taskName];
+                }
+                sorted.push([i, totalScore]);
+            }
+            sorted.sort(function(a, b) { return b[1] - a[1] });
+
+            for (var i = 0; i < sorted.length; ++i) {
+                sorted[i] = users.users[sorted[i][0]]
+            }
+
+            variables = { 'tasks' : tasks.tasks, 'users' : sorted};
+            res.render('ratings', variables);
+        });
+    });
 }
